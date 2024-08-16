a36d713f18360b276be7a010c29b43b49e668238
djb950 <ba306303@gmail.com>
2024-08-16 19:57:03 -0400

    Initial Commit

---
+++ Meal DB/Meal_DBApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_DBApp.swift
+//  Meal DB
+//
+//  Created by DJ A on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Meal_DBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meal DBTests/Meal_DBTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_DBTests.swift
+//  Meal DBTests
+//
+//  Created by DJ A on 8/16/24.
+//
+
+import Testing
+@testable import Meal_DB
+
+struct Meal_DBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
